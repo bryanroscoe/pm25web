2d8ea9cbfb7c4f46691d3a5e14c4e286da101844
Team Lary <teamlary@davidlary.info>
2013-03-08 19:58:56 -0600

    Added new optoins for graph, better mobile

--- public_html/js/pm25web.js
+++ public_html/js/pm25web.js
@@ -1,4 +1,4 @@
-$('#graph').css('margin-left', '-50px');
+$('#graph').css('margin-left', '-20px');
 //$('#graph').css('display', 'block');
 
 
@@ -97,31 +97,28 @@ function showPosition(city, state, latitude, longitude)
             //console.log(errorThrown);
         },
         success: function(data){
-            var contents = null;
-            //makeGraph(data);
-            $.get('/' + data, contents,makeGraph, 'text');
-            //alert(data);
             
             g = new Dygraph(
                 document.getElementById("graph"),
                 data,
                 {           
                     labels: ["Date", "Lat", "Long", "PM2.5"],
-                    visibility: [false, false, true]
+                    visibility: [false, false, true],
+                    interactionModel: {},
+                    panEdgeFraction: 0,
+                    hideOverlayOnMouseOut: false,
+                    fillGraph: true,
+                    colors: ['blue'],
+                    yAxisLabelWidth: 20,
+                    axisLabelWidth:20
+
+
+
                 }
             );
             
         }   
     });
-}
-function makeGraph(content)
-{
-    console.log(content );
-    console.log(content)
-    pm25data = CSVToArray(content);
-    console.log($.csv.toArray(content, {delimiter: ''}));
-    
-
 }
 
 function getAjaxPostion(error)
@@ -157,88 +154,4 @@ if (navigator.geolocation)
 else
 {
     getAjaxPostion(null);
-}
-  // This will parse a delimited string into an array of
-    // arrays. The default delimiter is the comma, but this
-    // can be overriden in the second argument.
-function CSVToArray( strData, strDelimiter ){
-    // Check to see if the delimiter is defined. If not,
-    // then default to comma.
-    strDelimiter = (strDelimiter || ",");
-
-    // Create a regular expression to parse the CSV values.
-    var objPattern = new RegExp(
-            (
-             // Delimiters.
-             "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-
-             // Quoted fields.
-             "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-
-             // Standard fields.
-             "([^\"\\" + strDelimiter + "\\r\\n]*))"
-            ),
-            "gi"
-            );
-
-
-    // Create an array to hold our data. Give the array
-    // a default empty first row.
-    var arrData = [[]];
-
-    // Create an array to hold our individual pattern
-    // matching groups.
-    var arrMatches = null;
-
-
-    // Keep looping over the regular expression matches
-    // until we can no longer find a match.
-    while (arrMatches = objPattern.exec( strData )){
-
-        // Get the delimiter that was found.
-        var strMatchedDelimiter = arrMatches[ 1 ];
-
-        // Check to see if the given delimiter has a length
-        // (is not the start of string) and if it matches
-        // field delimiter. If id does not, then we know
-        // that this delimiter is a row delimiter.
-        if (
-                strMatchedDelimiter.length &&
-                (strMatchedDelimiter != strDelimiter)
-           ){
-
-            // Since we have reached a new row of data,
-            // add an empty row to our data array.
-            arrData.push( [] );
-
-        }
-
-
-        // Now that we have our delimiter out of the way,
-        // let's check to see which kind of value we
-        // captured (quoted or unquoted).
-        if (arrMatches[ 2 ]){
-
-            // We found a quoted value. When we capture
-            // this value, unescape any double quotes.
-            var strMatchedValue = arrMatches[ 2 ].replace(
-                    new RegExp( "\"\"", "g" ),
-                    "\""
-                    );
-
-        } else {
-
-            // We found a non-quoted value.
-            var strMatchedValue = arrMatches[ 3 ];
-
-        }
-
-
-        // Now that we have our value string, let's add
-        // it to the data array.
-        arrData[ arrData.length - 1 ].push( strMatchedValue );
-    }
-
-    // Return the parsed data.
-    return( arrData );
 }
